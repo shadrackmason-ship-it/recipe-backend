d9d25274365c7ebb21bd711fd20f113a9b1e2e07
Sahal mohamed <sahal.mohamed1@student.moringaschool.com>
2026-06-12 22:26:12 +0300

    Added get by id feature

--- routes/recipes.py
+++ routes/recipes.py
@@ -20,10 +20,28 @@ recipes = []
 def get_recipes():
     return {"recipes": recipes}
 
+@router.get("/{recipe_id}")
+def get_recipe(recipe_id: int):
+    for recipe in recipes:
+        if recipe["id"] == recipe_id:
+            return recipe
+    return {
+        "message": "Recipe not found"
+    }
+
 @router.post("/")
 def create_recipe(recipe: Recipe):
-    recipes.append(recipe.model_dump())
+    new_recipe = {
+        "id": len(recipes) + 1,
+        "title": recipe.title,
+        "description": recipe.description,
+        "category": recipe.category,
+        "prep_time": recipe.prep_time,
+        "cook_time": recipe.cook_time,
+        "servings": recipe.servings
+    }
+    recipes.append(new_recipe)
     return {
         "message": "Recipe created",
-        "recipe": recipe
+        "recipe": new_recipe
     }
