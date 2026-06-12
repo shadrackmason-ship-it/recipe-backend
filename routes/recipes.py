a3c57156be931c2b0f02a4f851092b154b7fe7e5
Sahal mohamed <sahal.mohamed1@student.moringaschool.com>
2026-06-12 21:53:57 +0300

    Added post schema

--- routes/recipes.py
+++ routes/recipes.py
@@ -1,10 +1,29 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter(
     prefix = "/recipes",
     tags = ["Recipes"]
 )
 
+class Recipe(BaseModel):
+    title: str
+    description: str
+    category: str
+    prep_time: int
+    cook_time: int
+    servings: int
+
+recipes = []
+
 @router.get("/")
 def get_recipes():
-    return {"recipes": []}
+    return {"recipes": recipes}
+
+@router.post("/")
+def create_recipe(recipe: Recipe):
+    recipes.append(recipe.dict())
+    return {
+        "message": "Recipe created",
+        "recipe": recipe
+    }
