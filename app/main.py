71b09aee2e6b5d32a157ea100bf8b6ed8170e54e
samsonmorara-maker/samsongit config  --global user.email samson.morara@student.moringaschool.com <samson.morara@student.moringaschool.com>
2026-06-13 20:13:49 +0300

    FIXING ERRORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 from app.database import Base, engine
 from app.routes import auth
+
+
 Base.metadata.create_all(Bind=engine)
+
 app = FastAPI(title="Recipe Backend")
-app.include_router(auth.router)
+
+@app.get("/")
+def root():
+    return {"message": "Recipe backend is running"}
+
+app.include_router(auth.router,prefix="/api")
