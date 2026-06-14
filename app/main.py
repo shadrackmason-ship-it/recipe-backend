619407934defbdbf55e408d248eed46abc9c6c24
samsonmorara-maker/samsongit config  --global user.email samson.morara@student.moringaschool.com <samson.morara@student.moringaschool.com>
2026-06-14 17:28:10 +0300

    signing up error

--- app/main.py
+++ app/main.py
@@ -2,11 +2,22 @@ from fastapi import FastAPI
 from app.database import Base, engine
 from routes import auth
 
+from fastapi.middleware.cors import CORSMiddleware
+
 
 Base.metadata.create_all(bind=engine)
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="Recipe Backend")
+from fastapi.middleware.cors import CORSMiddleware
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def root():
     return {"message": "Recipe backend is running"}
