e878df0d29652675a153d8b44a9b82cd8d8d2252
samsonmorara-maker/samsongit config  --global user.email samson.morara@student.moringaschool.com <samson.morara@student.moringaschool.com>
2026-06-15 11:50:01 +0300

    merging conflict

--- app/main.py
+++ app/main.py
@@ -19,6 +19,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 app.include_router(auth_router)
 app.include_router(search_router)
 app.include_router(categories_router)
