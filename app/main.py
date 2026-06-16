1c3f485dc4f0b28ddd5df7c66c936ae288541b84
shadrack mason <shadrack.mason@student.moringaschool.com>
2026-06-16 21:51:21 +0300

    fix cors

--- app/main.py
+++ app/main.py
@@ -16,16 +16,13 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://localhost:5174",
         "http://127.0.0.1:5173",
-        "http://127.0.0.1:5174",
         "https://fabulous-beijinho-cce89f.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 app.include_router(auth_router)
 app.include_router(recipes_router)
 app.include_router(search_router)
