b818d8bdb6bed2b4c50eb81ff63e593c74397557
shadrack mason <shadrack.mason@student.moringaschool.com>
2026-06-16 17:35:35 +0300

    final commit

--- app/main.py
+++ app/main.py
@@ -18,7 +18,8 @@ app.add_middleware(
         "http://localhost:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5173",
-        "http://127.0.0.1:5174"
+        "http://127.0.0.1:5174",
+        "https://fabulous-beijinho-cce89f.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
